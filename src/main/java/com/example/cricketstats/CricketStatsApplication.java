86630c08b38a83a5b3d795aa911c0460012d42c3
Chris Hern <chrishern@hotmail.com>
2025-09-15 21:38:42 +0100

    Get app running against Java 24

--- src/main/java/com/example/cricketstats/CricketStatsApplication.java
+++ src/main/java/com/example/cricketstats/CricketStatsApplication.java
@@ -1,9 +1,11 @@
+package com.example.cricketstats;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.example.cricketstats")
-public class Main {
+public class CricketStatsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(CricketStatsApplication.class, args);
     }
 }
